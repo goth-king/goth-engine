b87e879bb0e157e690edbdf90acd39cd367d1158
goth-king <122706209+goth-king@users.noreply.github.com>
2023-02-17 23:51:19 -0800

    modified ladder class
    renamed class script
    changed code for player to climb ladder

---
+++ resources/class_scripts/class_ladder.gd
@@ -0,0 +1,13 @@
+class_name StaticBodyLadder
+extends StaticBody3D
+
+
+@export var ladder_mesh : MeshInstance3D
+var ladder_length : float
+
+func _ready():
+	
+	pass
+	
+func _process(delta):
+	pass
--- resources/hero.gd
+++ resources/hero.gd
@@ -3,22 +3,18 @@ extends CharacterBody3D
 @export var speed : float = 5.0
 @export var jump : float = 5.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") / ProjectSettings.get_setting("physics/common/physics_ticks_per_second")
-
-
-enum STATE {STANDING, MOVING, FALLING}
-enum ACTION {ATTACK, JUMP, LAND}
-var state = STATE.STANDING
 var action = null
 
 
 @onready var animplayer : AnimationPlayer = $AnimationPlayer
+@onready var statemachine : StateMachine = $HeroStateMachine
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
 
@@ -27,86 +23,21 @@ func _physics_process(delta):
 	var movement = get_movement_input()
 	look_at(global_transform.origin + movement)
 	
-	action = get_action_input()
-
-	match state:
-		STATE.STANDING:
-			state = state_standing(movement)
-		STATE.MOVING:
-			state = state_moving(movement)
-		STATE.FALLING:
-			state = state_falling(movement)
-			
-
 	
 	velocity.x = speed * movement.x
 	velocity.z = speed * movement.z
 	if is_on_wall():
 		var collider = get_last_slide_collision().get_collider()
-		var collider_class
-		if collider.has_method("get_extended_class"):
-			collider_class = collider.get_extended_class()
-			if collider_class == "Ladder":
+		var collider_shape = get_last_slide_collision().get_collider_shape().shape
+		if collider is StaticBodyLadder:
+			if transform.origin.y < (collider.transform.origin.y + collider_shape.size.y - 1):
 				velocity.x = 0
 				velocity.z = 0
-				velocity.y = Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
-			
+				velocity.y = Input.get_action_strength("move_up") - Input.get_action_strength("move_down")#
 			
 	else:
 		velocity.y -= gravity
 	move_and_slide()
-	
-
-func state_standing(movement):
-		
-	if not is_on_floor():
-		return STATE.FALLING
-	elif movement.length() > 0:
-		return STATE.MOVING
-		
-	match action:
-		ACTION.JUMP:
-			animplayer.play("jump")
-		ACTION.ATTACK:
-			animplayer.play("attack")
-			print("attacking while standing")
-
-
-	return STATE.STANDING
-
-		
-
-	
-func state_moving(movement): 
-	
-	if not is_on_floor():
-		return STATE.FALLING
-	elif movement.length() > 0:
-		return STATE.STANDING
-		
-	match action:
-		ACTION.JUMP:
-			animplayer.play("jump")
-		ACTION.ATTACK:
-			animplayer.play("attack")
-			print("attacking while moving")
-	
-	return STATE.MOVING
-
-
-		
-
-	
-func state_falling(movement):
-	
-	if is_on_floor() and movement.length() > 0:
-		return STATE.MOVING
-	elif is_on_floor():
-		return STATE.STANDING
-	
-	pass
-
-
 	
 
 
@@ -127,19 +58,3 @@ func get_movement_input():
 		input = input.normalized()
 		
 	return input
-
-func get_action_input():
-	
-	if Input.is_action_just_pressed("attack"):
-		print("attack input received")
-		return ACTION.ATTACK
-		
-	if Input.is_action_just_pressed("jump"):
-		return ACTION.JUMP
-		
-		
-	pass
-
-
-func do_jump():
-	velocity.y = jump
