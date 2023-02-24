fdf2c40218d1a8a97eb46b6c5533398a7c61b98c
goth-king <122706209+goth-king@users.noreply.github.com>
2023-02-23 21:55:42 -0800

    added state pattern movement
    STAND RUN CLIMB FALL movement states
    added state switching based on which class of object the character is colliding with

--- resources/hero.gd
+++ resources/hero.gd
@@ -2,15 +2,20 @@ extends CharacterBody3D
 
 @export var speed : float = 5.0
 @export var jump : float = 5.0
+@export var max_climb : float = PI/6
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") / ProjectSettings.get_setting("physics/common/physics_ticks_per_second")
 var action = null
 var input : Vector3
 var facing : Vector3
+var movement : Vector3
+var collider : CollisionObject3D
 
+enum STATE {STAND, RUN, CLIMB, FALL}
+var state = STATE.STAND
 
 @onready var animplayer : AnimationPlayer = $AnimationPlayer
 @onready var statemachine : StateMachine = $HeroStateMachine
-
 
 func _ready():
 	pass # Replace with function body.
@@ -19,64 +24,83 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	var movement = get_movement_input()
-	
-	if movement.length() > 0:
-		look_at(global_transform.origin + movement)
-		facing = movement.normalized()
-	
-	velocity.y -= gravity
-	velocity.x = speed * movement.x
-	velocity.z = speed * movement.z
-	
-	if is_on_wall():
-		var wall = get_last_slide_collision()
-		var wall_body = wall.get_collider()
-		var wall_height = wall.get_collider_shape().shape.get_debug_mesh().get_aabb().size.y
-		var wall_normal = get_wall_normal()
-		var climb_movement
-		
-		#pushing toward the wall causes climbing up, pulling away causes climbing down
-		#will be replaced with code only triggering climbing if pushing against wall at a near perpendicular angle
-		if movement.dot(wall_normal) < 0:
-			climb_movement = movement.length()
-		else:
-			climb_movement = -1 * movement.length()
-		
-		#Will be replaced with a state machine
-		#clean up movement (make character climb straight up the center of the ladder, straight up ledges), tie into animationtree
-		if wall_body is Platform:
-			velocity.y = climb_movement
-			velocity.x = 0
-			velocity.z = 0
-		if wall_body is Ladder: 
-			velocity.y = climb_movement
-			velocity.x = 0
-			velocity.z = 0
-		elif wall_body is Stairs:
-			velocity.y = climb_movement
-		else:
-			velocity.y = climb_movement
+	set_collider()
 	
 
-	
-	move_and_slide()
-	
-#rotates movement input based on camera view angle
-func get_movement_input():
-	
-	var orientation:Vector3
-	var camera = get_viewport().get_camera_3d()
-	
-	input.x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
-	input.z = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	orientation.x = camera.position.x - position.x
-	orientation.z = camera.position.z - position.z
-	
-	input = (orientation * input.z) + (orientation.cross(Vector3.UP) * input.x)
-	
-	if input.length() > 1:
-		input = input.normalized()
+	movement = PlayerInput.get_movement_vector()
+			
+	match state:
+		STATE.STAND:
+			state = standing()
+		STATE.RUN:
+			state = running()
+		STATE.CLIMB:
+			state = climbing()
+		STATE.FALL:
+			state = falling()
+
+func standing():
+	if not is_on_floor():
+		return STATE.FALL
+	if movement.length() > 0:
+		return STATE.RUN
+	return STATE.STAND
+
+func running():
+	if collider is Ladder:
+		return STATE.CLIMB
+	if movement.length() == 0:
+		return STATE.STAND
+	else:		
+		velocity.y -= gravity
+		velocity.x = speed * movement.x
+		velocity.z = speed * movement.z
+		move_and_slide()
+		return STATE.RUN
+
+#climbing doesn't work
+func climbing():
+	if collider is Ladder:
+		velocity.y = 1
+		move_and_slide()
+		return STATE.CLIMB
+	else:
+		return STATE.FALL
 		
-	return input
+		
+func falling():
+	move_and_slide()
+	if is_on_floor():
+		return STATE.STAND
+	else:
+		velocity.y -= gravity
+		return STATE.FALL
+	
+
+#reads all collisions and sets collider to the most relevant collider class
+#needs cleanup
+func set_collider():
+	var new_collider
+	var collision
+	var touched = false
+	
+	#looks for collisions with specified classes
+	for i in range(get_slide_collision_count()):
+		collision = get_slide_collision(i)
+		new_collider = collision.get_collider()
+		if touched == false:
+			if (collider != new_collider) and (new_collider is Ladder or new_collider is Platform):
+				collider = new_collider
+				touched = true
+				print(Time.get_ticks_msec()," ", collider, " touched collider")
+	
+	#if no specified classes were found, looks for floor
+	if touched == false:
+		if is_on_floor_only():
+			collision = get_last_slide_collision()
+			if collision:
+				collider = collision.get_collider()
+	
+	#if no floor is found, clears collider
+	if not is_on_ceiling() and not is_on_floor():
+		collider = null
